972816bb0572ca2232333aaa53b2d31a725589d5
Brian Cortes <briancorts166@gmail.com>
2018-08-09 18:44:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
+var getFirstSelector = function(selector) {
+  return document.querySelector(selector)
+}
 
+var nestedTarget = function() {
+ return document.querySelector('div.target')
+}
+
+
+var increaseRankBy = function(n) {
+  var list = document.getElementById('app').querySelectorAll('ul.ranked-list li')
+
+  for (let i = 0; i < list.length; i++) {
+
+    list[i].innerHTML = (parseInt(list[i].innerHTML, 10) + n).toString()
+  }
+}
+
+var deepestChild = function() {
+  var list = document.getElementById('app').querySelector('div.grand-node')
+console.log(list)
+}
